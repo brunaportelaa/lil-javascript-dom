d635354a1d204bf8976fb642c897fc1090a625da
brunaportelaa <brunaporteladj@gmail.com>
2024-04-15 15:20:22 -0300

    Challenge solved for chapter 3, lesson 5

--- 3 - Traversing the DOM/Challenge/Challenge/script.js
+++ 3 - Traversing the DOM/Challenge/Challenge/script.js
@@ -1,6 +1,16 @@
 // Your tasks:
 // 1. Find the parent element of a cat and log it to the console.
+const ourCat = document.querySelector('li:nth-child(2)')
+console.log(ourCat)
+const catFather = ourCat.parentNode
+console.log(catFather)
 
 // 2. List all the children of the cat’s parent and log them.
+let kittens = catFather.children
+kittens = Array.from(kittens)
+console.log(kittens)
+kittens.forEach (kitten => console.log(kitten))
 
 // 3. Identify the cat’s parent's next sibling in the tree and log it.
+const catUncle = catFather.nextSibling
+console.log(catUncle)
