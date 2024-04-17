dd1f46b3868a05901eeb5d682e24a12c62ea5d50
brunaportelaa <brunaporteladj@gmail.com>
2024-04-17 09:27:10 -0300

    Added my exercise solution to folder 'Challenge'
    Added my exercise solution to folder 'Challenge' so I could compare with solution provided by author on folder 'Solution'

--- 5 - Creating, adding, and managing nodes/Challenge/Challenge/script.js
+++ 5 - Creating, adding, and managing nodes/Challenge/Challenge/script.js
@@ -1,15 +1,28 @@
 // Challenge Instructions:
 // 1. Craft and add a new button element with the text “Add” to the content area.
-//    Hint: Use createElement and appendChild.
+const btn = document.createElement('button')
+btn.textContent = "Add"
+const contentArea = document.getElementById('contentArea')
+contentArea.appendChild(btn)
 
 // 2. Summon a new list item and add it to the existing list 'taskList'.
-//    Hint: Create an 'li' element and use appendChild.
+const newItem = document.createElement('li')
+const existingItem = document.querySelector('li')
+newItem.textContent = 'Prioritary task'
+existingItem.parentNode.insertBefore(newItem, existingItem)
+
 
 // 3. Clone an element and add its twin to the page.
-//    Hint: Use cloneNode on an existing element.
+const dog = document.getElementById('puppyImage')
+const secondDog = dog.cloneNode()
+dog.parentNode.appendChild(secondDog)
 
 // 4. Replace the old paragraph at the top of the page with a new one.
-//    Hint: Create a new 'p' element and use replaceChild.
+const paragraph = document.querySelector('p')
+const newParagraph = document.createElement('p')
+newParagraph.innerText = "Hey, I'm the new paragraph!"
+paragraph.parentNode.replaceChild(newParagraph, paragraph)
 
 // 5. Make the image of the puppy vanish from the page. (I know, terrible!)
-//    Hint: Use removeChild on the parent of the 'puppyImage'.
+dog.parentNode.removeChild(dog)
+//Don't worry, I kept the second one
