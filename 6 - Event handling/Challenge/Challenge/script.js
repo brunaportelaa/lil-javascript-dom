d8b86e3819709bcf22e36d722036d292e8b7847e
brunaportelaa <brunaporteladj@gmail.com>
2024-04-17 20:54:33 -0300

    Exercise solved for chapter 6

--- 6 - Event handling/Challenge/Challenge/script.js
+++ 6 - Event handling/Challenge/Challenge/script.js
@@ -1,13 +1,30 @@
 document.addEventListener('DOMContentLoaded', () => {
     // Task: React to a button click by changing text in the paragraph with id 'para1'.
-    // Add event listener to 'changeTextButton' and modify text of 'para1' when clicked.
+   const btn =  document.getElementById('changeTextButton')
+   const p = document.getElementById('para1')
+   btn.addEventListener('click', () => p.textContent = 'Hover to change style :)')
+
 
     // Task: Change the style of 'para1' on mouseover and mouseout.
-    // Add mouseover and mouseout event listeners to 'para1' and change its style.
+    function styleParagraph () {
+        p.style.fontWeight = 'bold'
+        p.style.color = 'red'
+    }
+
+    function resetStyle() {
+        p.style.fontWeight = '300'
+        p.style.color = 'black'
+    }
+
+    p.addEventListener ('mouseover', styleParagraph)
+    p.addEventListener ('mouseout', resetStyle)
 
     // Task: Use event delegation on 'itemList' to display which item was clicked.
-    // Add click event listener to 'itemList' and identify the clicked list item.
+    const itemList = document.getElementById('preventLink')
+    itemList.addEventListener('click', function(e) {e.target.textContent = 'Item Clicked!'})
+
 
     // Task: Prevent navigating to the link URL with preventDefault and show text in 'para2'.
-    // Add click event listener to 'preventLink' and prevent default action.
+    const p2 = document.getElementById('preventLink')
+    p2.addEventListener('click', e => e.preventDefault())
 });
