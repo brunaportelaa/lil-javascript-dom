356ec81e0ca757a4988e9b4fb6be07b9ea3bf73a
brunaportelaa <brunaporteladj@gmail.com>
2024-04-18 16:42:16 -0300

    Challenge solved for chapter 7

--- 7 - Advanced DOM manipulation/Challenge/Challenge/script.js
+++ 7 - Advanced DOM manipulation/Challenge/Challenge/script.js
@@ -7,13 +7,40 @@ document.addEventListener('DOMContentLoaded', () => {
 
     form.addEventListener('submit', (e) => {
         e.preventDefault();
+
+        let nameFeedback = document.createElement('p')
+        form.insertBefore(nameFeedback, nameInput.nextSibling)
+
+        let emailFeedback = document.createElement('p')
+        form.insertBefore(emailFeedback, emailInput.nextSibling)
+
+
+        let passwordFeedback = document.createElement('p')
+        form.insertBefore(passwordFeedback, passwordInput.nextSibling)
         
         // Task: Validate each field here
         // - Check if the name, email, and password are not empty.
         // - Validate the email format.
         // - Check the password length and complexity.
         // - Display messages or indicators for each input's validation.
+        if (nameInput.value.trim() === '') {
+            nameFeedback.textContent = 'Name cannot be empty'
+            nameFeedback.style.color = 'red'
+        }
 
+        if (emailInput.value.trim() === '') {
+            emailFeedback.textContent = 'Email cannot be empty'
+            emailFeedback.style.color = 'red'
+        }
+
+        if (passwordInput.value.trim() === '') {
+            passwordFeedback.textContent = 'Password cannot be empty'
+        }
+
+        if (passwordInput.value.length < 8) {
+            passwordFeedback.textContent = 'Password must have at least 8 characters'
+            passwordFeedback.style.color = 'red'
+        }
         // Example Validation:
         // if (nameInput.value.trim() === '') {
         //     formFeedback.textContent = 'Name cannot be empty';
