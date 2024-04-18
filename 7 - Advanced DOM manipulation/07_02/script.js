dddb9a3e1881cc1c0ab0c2f2cb79a4875ff09404
brunaportelaa <brunaporteladj@gmail.com>
2024-04-17 21:38:51 -0300

    Exercise solved for chapter 7, lesson 2

--- 7 - Advanced DOM manipulation/07_02/script.js
+++ 7 - Advanced DOM manipulation/07_02/script.js
@@ -4,4 +4,23 @@ document.addEventListener('DOMContentLoaded', () => {
     const addTaskButton = document.getElementById('addTaskButton');
     const removeFirstTaskButton = document.getElementById('removeFirstTaskButton');
     const prioritizeLastTaskButton = document.getElementById('prioritizeLastTaskButton');
+
+
+
+    addTaskButton.addEventListener('click', () => {
+        let newListItem = document.createElement('li')
+        newListItem.textContent = newTaskInput.value
+        todoList.appendChild(newListItem)
+    })
+
+    removeFirstTaskButton.addEventListener('click', () => {
+        let firstItem =  todoList.querySelector('li')
+       todoList.removeChild(firstItem)
+    })
+
+    prioritizeLastTaskButton.addEventListener('click', () => {
+        let firstItem =  todoList.querySelector('li')
+        let lastItem = todoList.lastChild
+        todoList.insertBefore(lastItem, firstItem)
+    })
 });
