a8791826a107fdf1cc0ab209dea0c398d3128c69
brunaportelaa <brunaporteladj@gmail.com>
2024-04-18 14:10:54 -0300

    Exercise solved for chapter 7, lesson 3

--- 7 - Advanced DOM manipulation/07_03/script.js
+++ 7 - Advanced DOM manipulation/07_03/script.js
@@ -8,5 +8,29 @@ document.addEventListener('DOMContentLoaded', () => {
 
     const table = document.getElementById('employeeTable');
 
+    let thead = table.createTHead()
+    let row = thead.insertRow()
+    let headers = Object.keys(employees[0]).map(key => key.charAt(0).toUpperCase() + key.slice(1))
+    console.log(headers)
+
+    headers.forEach (text => {
+        let th = document.createElement('th')
+        th.textContent = text
+        row.appendChild(th)
+    })
+
+
+    let tbody = table.createTBody()
+    employees.forEach(emp => {
+        let row = tbody.insertRow()
+        Object.values(emp).forEach( text => {
+            let cell = row.insertCell()
+            cell.textContent = text
+        })
+    })
+
+
+
+
     
 });
